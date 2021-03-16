de8220b78944d2075096533eae67676a7e830798
gamercraze1 <64150160+gamercraze1@users.noreply.github.com>
2021-03-17 02:36:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+
+var engine,world;
+var weight,speed,calculation
+var car,wall 
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,400);
+  fill(80,80,80);
+  wall=createSprite(1500,200,60,height/2);  
+  
+  car=createSprite(50,200,50,50);
+  speed=random(55,90);
+  weight=random(400,1500)
+  
+  car.velocityX=speed
+  deformation();
+  
 }
 
 function draw() {
+
+  calculation=(0.5*weight*speed*speed)/22500
+  if(car.isTouching(wall)){
+    car.velocityX=0
+    
+  }
   background(255,255,255);  
   drawSprites();
+}
+
+function deformation(){
+if(calculation==0){
+ car.shapecolor =color(0,0,0)
+}else if(calculation<100&&calculation>0){
+  car.shapecolor =color(0,255,0)
+}else if(calculation>100&&calculation<180){
+  car.shapecolor =color(220,220,0)
+}else if(calculation>180){
+  car.shapecolor =color(255,0,0)
+}
+
 }
